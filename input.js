f379b4265e83af99021445d78d027a3620039ac8
denizedaza <dazadenize@gmail.com>
2021-08-25 15:50:31 -0400

    added wasd movements and connection for functions

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
+// Stores the active TCP connection object.
+let connection;
 
-
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,6 +15,22 @@ const handleUserInput = function(input) {
  if (input === '\u0003') {
     process.exit();
   }
+
+  if (input === 'w') {
+    connection.write("Move: up");
+  }
+  if (input === 'a') {
+    connection.write("Move: left");
+  }
+  if (input === 's') {
+    connection.write("Move: down");
+  }
+  if (input === 'd') {
+    connection.write("Move: right");
+  }
+
+   connection.write(`Input was: ${input}`);
+
 };
 
 module.exports = { setupInput }
