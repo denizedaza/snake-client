140e05e3d349b1f437cbc6da45b8a2c1a199e691
denizedaza <dazadenize@gmail.com>
2021-08-25 13:53:12 -0400

    created client file and destructured export lines

--- play.js
+++ play.js
@@ -5,24 +5,9 @@
 // Zoom Link to Snek UI: https://us02web.zoom.us/j/86924421191?pwd=UTZUeXVLbFd2UWZQdVVJbFFCQTZCZz09
 // Pass:  962883
 
-const net = require("net");
+const { connect } = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133', // IP address here,
-    port: 50542// PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log('Message from server:', data)
-  })
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
