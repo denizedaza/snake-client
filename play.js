544ced6c431b9a921521bb1e9236c8b89866c236
denizedaza <dazadenize@gmail.com>
2021-08-25 15:12:40 -0400

    added handleUserInput function and escape keyboard commands

--- play.js
+++ play.js
@@ -7,7 +7,26 @@
 
 const { connect } = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = function(input) {
+ if (input === '\u0003') {
+    process.exit();
+  }
+
+
+
+ 
+};
+
+setupInput();
 
 console.log("Connecting ...");
 connect();
