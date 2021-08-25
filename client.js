dab18ca1b1756b9ed797e059995ec1516a813c95
denizedaza <dazadenize@gmail.com>
2021-08-25 16:29:57 -0400

    created constants file and fixed associated files accordingly

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const {IP, PORT} = require('./constants');
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.223.133', // IP address here,
-    port: 50542// PORT number here,
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
@@ -17,11 +18,6 @@ const connect = function () {
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write("Name: NZE");
-
-    // setInterval(() => {
-    //   conn.write("Move: up")
-    //   // conn.write
-    // }, 500)
   })
 
   return conn;
--- play.js
+++ play.js
@@ -1,10 +1,3 @@
-// 
-
-// Snek Server IP: 135.23.223.133  (corrected!)
-// Snek Port: 50542
-// Zoom Link to Snek UI: https://us02web.zoom.us/j/86924421191?pwd=UTZUeXVLbFd2UWZQdVVJbFFCQTZCZz09
-// Pass:  962883
-
 const { connect } = require('./client');
 const { setupInput } = require("./input");
 
